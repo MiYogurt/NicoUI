87d09ec1ecfaccacff16acdac687543aa8894545
ChenMingMing <belovedyogurt@gmail.com>
2018-07-05 16:08:42 +0800

    📺  第十二期完成

--- index.ts
+++ index.ts
@@ -19,3 +19,20 @@ if (module.hot) {
     window.location.reload()
   })
 }
+
+toggleClass('#show-modal', '.modal', 'show')
+toggleClass('.modal .close', '.modal', 'show')
+toggleClass('.modal .bg', '.modal', 'show')
+
+const menu = () => {
+  ;(Array as any).from(document.querySelectorAll('.menu .title')).map(dom => {
+    console.log(dom)
+    dom.onclick = () => {
+      dom.classList.toggle('show')
+
+      dom.nextElementSibling.classList.toggle('show')
+    }
+  })
+}
+
+menu()
